3b81dde2013d716396a7cce34d3c0bfb7e31043c
epam <epam@local.rsu.edu.ru>
2018-10-09 20:56:46 +0300

    sec

---
+++ src/ex2_js-basics-part2/task02.js
@@ -0,0 +1,10 @@
+var arr = [];
+function f(arr) {
+    for (var i =0; i<arr.length; i++) {
+        console.log(arr[i]);
+    }
+    console.log('общее число элементов: ', arr.length);
+    }
+
+
+    f([1,1,12,22])
---
+++ src/ex3_js-objects-part1/task01.js
@@ -0,0 +1,5 @@
+var obj = {}
+obj.a = 100;
+obj.b = 111;
+obj.c = 122;
+delete obj.c
